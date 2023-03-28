1c5127d4d5e270ed9b7e39a7c07ce36acbdbb569
lequoc2877 <21521357@gm.uit.edu.vn>
2023-03-28 16:40:35 +0700

    make locsys.py testable

--- src/data/localisationssystem/locsys.py
+++ src/data/localisationssystem/locsys.py
@@ -3,10 +3,16 @@ from multiprocessing import Pipe
 from threading import Thread
 
 
-from src.data.localisationssystem.server_data import ServerData
-from src.data.localisationssystem.server_listener import ServerListener
-from src.data.localisationssystem.server_subscriber import ServerSubscriber
-from src.data.localisationssystem.position_listener import PositionListener
+if __name__=='__main__':
+    from server_data import ServerData
+    from server_listener import ServerListener
+    from server_subscriber import ServerSubscriber
+    from position_listener import PositionListener
+else:
+    from src.data.localisationssystem.server_data import ServerData
+    from src.data.localisationssystem.server_listener import ServerListener
+    from src.data.localisationssystem.server_subscriber import ServerSubscriber
+    from src.data.localisationssystem.position_listener import PositionListener
 from PIL import Image
 import ast
 import json
@@ -24,7 +30,7 @@ class LocalisationSystem(Thread):
         """ GpsTracker targets to connect on the server and to receive the messages, which incorporates 
         the coordinate of the robot on the race track. It has two main state, the setup state and the listening state. 
         In the setup state, it creates the connection with server. It's receiving  the messages from the server in the listening
-        state. 
+        state. on
 
         It's a thread, so can be running parallel with other threads. You can access to the received parameters via 'coor' function.
 
@@ -70,18 +76,18 @@ class LocalisationSystem(Thread):
         self.__position_listener.stop()
 
 if __name__ == '__main__':
-    parser = GraphMLParser()
-    map = parser.parse('Test_track.graphml')
-    x=[]
-    y=[]
-    for node in map.nodes():
-        x.append(node['d0'])
-        y.append(node['d1'])
-    x = np.array(x).astype(float)
-    y = np.array(y).astype(float)
+    # parser = GraphMLParser()
+    # map = parser.parse('Test_track.graphml')
+    # x=[]
+    # y=[]
+    # for node in map.nodes():
+    #     x.append(node['d0'])
+    #     y.append(node['d1'])
+    # x = np.array(x).astype(float)
+    # y = np.array(y).astype(float)
     beacon = 12345
     id = 0x1705
-    serverpublickey = 'publickey_server_test.pem'
+    serverpublickey = 'src/data/localisationssystem/publickey_server_test.pem'
     gpsStR, gpsStS = Pipe(duplex = False)
 
     LocalisationSystem = LocalisationSystem(id, beacon, serverpublickey, gpsStS)
@@ -98,8 +104,8 @@ if __name__ == '__main__':
     # fig.canvas.draw()
     # img =Image.frombytes('RGB', fig.canvas.get_width_height(),fig.canvas.tostring_rgb())
     # img.save('image.jpg')
-    map_arr=[[x,y]for x,y in zip(x,y)]
-    np.save('map.npy',map_arr)
+    # map_arr=[[x,y]for x,y in zip(x,y)]
+    # np.save('map.npy',map_arr)
     # dist_arr = euclidean_distances([[x_tag,y_tag]], map_arr)
     # print(np.argmin(dist_arr)+1)
     time.sleep(3)
@@ -111,9 +117,10 @@ if __name__ == '__main__':
             #mo file a.jpg de xem ket qua realtime
             x_tag,y_tag=coora['coor'][1].real,coora['coor'][0].real
             # plt.plot(x_tag,y_tag, marker="o",markerfacecolor='red', markersize=12)
-            data.append([x_tag,y_tag])       
-            dist_arr = euclidean_distances([[x_tag,y_tag]], map_arr)
-            print(np.argmin(dist_arr)+1)
+            # data.append([x_tag,y_tag])       
+            print(x_tag,y_tag)
+            # dist_arr = euclidean_distances([[x_tag,y_tag]], map_arr)
+            # print(np.argmin(dist_arr)+1)
             # fig.canvas.draw()
             # img =Image.frombytes('RGB', fig.canvas.get_width_height(),fig.canvas.tostring_rgb())
             # img.save('image.jpg')
@@ -121,7 +128,6 @@ if __name__ == '__main__':
             sleep(0.005)            
         except KeyboardInterrupt:
             break
-    np.save('data.npy',data)
     LocalisationSystem.stop()
 
     LocalisationSystem.join()
