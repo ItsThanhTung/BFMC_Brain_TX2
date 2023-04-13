03f105ad7e2612174d1a565eefd0d1dba88152fc
ItsThanhTung <tung.10hoa.2016@gmail.com>
2023-04-13 13:48:45 +0700

    merger EKF

--- src/utils/SensorProcess/utils.py
+++ src/utils/SensorProcess/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 def GetIMUHeading(DataJson):
-    return np.deg2rad(DataJson["IMU"]["Euler"][0] - 110)
+    return np.deg2rad(DataJson["IMU"]["Euler"][0] +178)
 
 
 def GetInitalData(DataJson):
