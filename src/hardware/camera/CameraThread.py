a91e5ab4c1eb602d744e5b094f29a60aaabbae42
lequoc2877 <21521357@gm.uit.edu.vn>
2023-04-28 14:41:17 +0700

    new roi

--- src/hardware/camera/CameraThread.py
+++ src/hardware/camera/CameraThread.py
@@ -143,6 +143,7 @@ class CameraThread(ThreadWithStop):
                                                     (0,height*0.8),
                                                     (self.opt["roi"]["left"]*width, height * self.opt["roi"]["upper"]),
                                                     (self.opt["roi"]["right"]*width, height * self.opt["roi"]["upper"]),
+                                                    (width-1,height*0.8),
                                                     (width - 1, height-1)]], np.int32)
 
         cv2.fillPoly(mask, region_of_interest_vertices, (255,255,255))
